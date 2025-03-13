b5964afb62baa1c273b45f70f0c4dd5bbd044ef8
justinwells85 <38437860+justinwells85@users.noreply.github.com>
2025-03-12 22:08:17 -0400

    Initial commit: Set up Spring Boot project with VS Code and package structure

---
+++ src/main/java/com/justinwells/xlsUploader/XlsUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.justinwells.xlsUploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XlsUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XlsUploaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/justinwells/xlsUploader/XlsUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justinwells.xlsUploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XlsUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
